20e0598f309ddea50b440db3e6927aaf44fe57f4
shinypipibear <72198214+shinypipibear@users.noreply.github.com>
2022-04-28 22:14:57 +0100

    Update component.js

--- database/models/component.js
+++ database/models/component.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const componentSchema = new Schema({
-    component_ID:{
-        type: Number,
-        required: true
-    },
     component_type:{
         type: String,
         required: true
